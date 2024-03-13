88fe29085870ea3300098ca5497338354e07a501
Larissa Corrêa <larimscorrea@gmail.com>
2024-03-13 20:14:34 -0300

    working

--- app1.py
+++ app1.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr} </h1>"
+
 @app.route("/test")
 def home():
     return render_template("new.html")
