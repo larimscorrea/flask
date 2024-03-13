0c5f60c759430f511207e234e5104a8d204e9c22
Larissa Corrêa <larimscorrea@gmail.com>
2024-03-13 20:09:57 -0300

    working

--- app1.py
+++ app1.py
@@ -2,11 +2,14 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
+@app.route("/")
+def home():
     return render_template("index.html")
 
+@app.route("/test")
+def home():
+    return render_template("new.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
